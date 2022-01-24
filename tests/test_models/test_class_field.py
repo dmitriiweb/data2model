bf1fc27b9c25c2093e215461b05acdd41384f18f
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-01-25 01:04:25 +0700

    feat: extract pythonic name from original name

--- tests/test_models/test_class_field.py
+++ tests/test_models/test_class_field.py
@@ -10,9 +10,13 @@ from data_to_model.models import ClassField
         ("Name", "name"),
         ("NameName", "name_name"),
         ("nameName", "name_name"),
-        ("NNNameNNameName", "nnn_ame_nn_ame_n_ame"),
-        ("+45Name", "_45name"),
+        ("+45Name", "_45_name"),
         ("45name", "_45name"),
+        ("name name", "name_name"),
+        ("name-name", "name_name"),
+        ("NNNameNNameName", "nnn_ame_nn_ame_name"),
+        ("namE", "name"),
+        ("name45", "name45"),
     ],
 )
 def test_name(original_name, expected_name):
