c1eb7679014c7b4d9b2f669e1240ec230bc6aeab
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-01-27 02:46:30 +0700

    tests: add cases for from_collection

--- tests/test_data_parsers/test_csv_parser.py
+++ tests/test_data_parsers/test_csv_parser.py
@@ -19,5 +19,21 @@ async def test_from_file(csv_test_file: pathlib.Path):
             assert field.type == "Union[float, str]"
 
 
-async def test_from_collection():
-    pass
+async def test_from_collection(csv_test_file: pathlib.Path):
+    data = []
+    async for row in CsvDataParser.read_csv(csv_test_file):
+        data.append(row)
+
+    parser = CsvDataParser()
+    data_classes = parser.from_collection(data, "Example")
+    data_class = data_classes[0]
+
+    assert data_class.name == "Example"
+
+    for field in data_class.fields:
+        if field.name.replace("\ufeff", "") == "col1":
+            assert field.type == "int"
+        elif field.name == "col2":
+            assert field.type == "Optional[float]"
+        elif field.name == "col3":
+            assert field.type == "Union[float, str]"
