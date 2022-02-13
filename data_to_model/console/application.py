c883f904213d882da7d22f1c05dafcd645389c9e
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-02-14 00:31:59 +0700

    refactor: make get user arguments function as method of Arguments

--- data_to_model/console/application.py
+++ data_to_model/console/application.py
@@ -9,40 +9,41 @@ from dataclasses import dataclass
 from pathlib import Path
 
 
-@dataclass()
 class Arguments:
-    input_file: Path
-    output_file: Path
+    def __init__(self, input_file: Path, output_file: Path):
+        self.input_file = input_file
+        self.output_file = output_file
 
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.input_file=}, {self.output_file=})"
 
-def get_user_args() -> Arguments:
-    parser = argparse.ArgumentParser(
-        description="Convert data file (csv, json etc. to Python model"
-    )
-    parser.add_argument(
-        "-if",
-        "--input-file",
-        type=str,
-        required=True,
-        help="Input file, e.g. data.csv",
-    )
-    parser.add_argument(
-        "-of",
-        "--output-file",
-        type=str,
-        required=True,
-        help="Output file, e.g. data.py",
-    )
+    @classmethod
+    def get(cls) -> "Arguments":
+        parser = argparse.ArgumentParser(
+            description="Convert data file (csv, json etc. to Python model"
+        )
+        parser.add_argument(
+            "-if",
+            "--input-file",
+            type=str,
+            required=True,
+            help="Input file, e.g. data.csv",
+        )
+        parser.add_argument(
+            "-of",
+            "--output-file",
+            type=str,
+            required=True,
+            help="Output file, e.g. data.py",
+        )
 
-    args = parser.parse_args()
+        args = parser.parse_args()
 
-    return Arguments(
-        input_file=Path(args.input_file), output_file=Path(args.output_file)
-    )
+        return cls(input_file=Path(args.input_file), output_file=Path(args.output_file))
 
 
 def main():
-    user_args = get_user_args()
+    user_args = Arguments.get()
 
 
 if __name__ == "__main__":
