6d792b33b940c4b00553ed18f107f0b264f4d984
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-02-14 01:21:26 +0700

    feat: add model generator

--- data_to_model/console/application.py
+++ data_to_model/console/application.py
@@ -3,12 +3,13 @@
 
 data2model -if data.csv -of data.py
 """
-import argparse
-import asyncio
+import asyncio  # noqa
 
 from pathlib import Path
 
 import asyncclick as click
+
+from data_to_model import ModelGenerator
 
 
 @click.command()
@@ -18,9 +19,20 @@ import asyncclick as click
 @click.option(
     "-of", "--output-file", type=str, required=True, help="Output file, e.g. data.py"
 )
-async def model_generator(input_file, output_file):
-    print(input_file)
-    print(output_file)
+@click.option(
+    "--csv-delimiter",
+    type=str,
+    required=False,
+    help="Delimiter in CSV files, default coma(',')",
+    default=",",
+)
+async def model_generator(input_file: str, output_file: str, csv_delimiter: str):
+    input_path = Path(input_file)
+    output_path = Path(output_file)
+
+    mg = ModelGenerator(input_path, csv_delimiter=csv_delimiter)
+    model = await mg.get_model()
+    await model.save(output_path)
 
 
 def main():
