321f0493a27119eea4556fadaaf7ea054e72af60
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-02-14 00:58:24 +0700

    feat: change argparser to asyncclick

--- data_to_model/console/application.py
+++ data_to_model/console/application.py
@@ -4,46 +4,27 @@
 data2model -if data.csv -of data.py
 """
 import argparse
+import asyncio
 
-from dataclasses import dataclass
 from pathlib import Path
 
+import asyncclick as click
 
-class Arguments:
-    def __init__(self, input_file: Path, output_file: Path):
-        self.input_file = input_file
-        self.output_file = output_file
 
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self.input_file=}, {self.output_file=})"
-
-    @classmethod
-    def get(cls) -> "Arguments":
-        parser = argparse.ArgumentParser(
-            description="Convert data file (csv, json etc. to Python model"
-        )
-        parser.add_argument(
-            "-if",
-            "--input-file",
-            type=str,
-            required=True,
-            help="Input file, e.g. data.csv",
-        )
-        parser.add_argument(
-            "-of",
-            "--output-file",
-            type=str,
-            required=True,
-            help="Output file, e.g. data.py",
-        )
-
-        args = parser.parse_args()
-
-        return cls(input_file=Path(args.input_file), output_file=Path(args.output_file))
+@click.command()
+@click.option(
+    "-if", "--input-file", type=str, required=True, help="Input file, e.g. data.csv"
+)
+@click.option(
+    "-of", "--output-file", type=str, required=True, help="Output file, e.g. data.py"
+)
+async def model_generator(input_file, output_file):
+    print(input_file)
+    print(output_file)
 
 
 def main():
-    user_args = Arguments.get()
+    model_generator(_anyio_backend="asyncio")
 
 
 if __name__ == "__main__":
